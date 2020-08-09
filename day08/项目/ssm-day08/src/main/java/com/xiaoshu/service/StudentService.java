0b13ee71ce9bbcd4e0f9a3d2acb6f8e16dc6abb7
root <123>
2020-08-09 22:53:19 +0800

    day08

---
+++ day08/项目/ssm-day08/src/main/java/com/xiaoshu/service/StudentService.java
@@ -0,0 +1,72 @@
+package com.xiaoshu.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.xiaoshu.dao.SchoolMapper;
+import com.xiaoshu.dao.StudentMapper;
+import com.xiaoshu.entity.School;
+import com.xiaoshu.entity.Student;
+import com.xiaoshu.entity.StudentExample;
+import com.xiaoshu.entity.StudentExample.Criteria;
+import com.xiaoshu.entity.StudentVo;
+
+@Service
+public class StudentService {
+
+	@Autowired
+	StudentMapper studentMapper;
+	
+	@Autowired
+	private SchoolMapper schoolMapper;
+
+
+	// 新增
+	public void addUser(Student t) throws Exception {
+		studentMapper.insert(t);
+	};
+
+	// 修改
+	public void updateUser(Student t) throws Exception {
+		studentMapper.updateByPrimaryKeySelective(t);
+	};
+
+	// 删除
+	public void deleteUser(Integer id) throws Exception {
+		studentMapper.deleteByPrimaryKey(id);
+	};
+
+	// 通过用户名判断是否存在，（新增时不能重名）
+	public Student existUserWithUserName(String userName) throws Exception {
+		StudentExample example = new StudentExample();
+		Criteria criteria = example.createCriteria();
+		criteria.andSnameEqualTo(userName);
+		List<Student> userList = studentMapper.selectByExample(example);
+		return userList.isEmpty()?null:userList.get(0);
+	};
+
+	
+
+	public PageInfo<StudentVo> findUserPage(StudentVo studentVo, int pageNum, int pageSize) {
+		PageHelper.startPage(pageNum, pageSize);
+		List<StudentVo> userList = studentMapper.findPage(studentVo);
+		PageInfo<StudentVo> pageInfo = new PageInfo<StudentVo>(userList);
+		return pageInfo;
+	}
+
+	public List<School> findSchool() {
+		// TODO Auto-generated method stub
+		return schoolMapper.selectAll();
+	}
+
+	public List<StudentVo> findPage(StudentVo studentVo) {
+		List<StudentVo> userList = studentMapper.findPage(studentVo);
+		return userList;
+	}
+
+
+}
